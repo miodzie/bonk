40c287b4b28bf9ccc3b41d802895380cf1c6359d
miodzie <miodzie@protonmail.com>
2021-11-16 22:28:47 -0500

    Basic sprite animation working.

--- src/sprites.js
+++ src/sprites.js
@@ -29,10 +29,6 @@ class Sprite {
     ele.appendChild(this.canvas);
   }
 
-  display() {
-    console.log('things happened')
-  }
-
   position(x, y) {
     if (!this.canvas) {
       console.error('Sprite: Canvas is null.')
@@ -43,26 +39,37 @@ class Sprite {
     this.canvas.getContext('2d').drawImage(this.image, x, y)
   }
 
-  play(seq) {
+  async play(seq) {
     let frames = seq.next()
-    this.position(frames.x, frames.y)
-    frames = seq.next()
-    this.position(frames.x, frames.y)
-    frames = seq.next()
-    this.position(frames.x, frames.y)
+    for (;;) {
+      this.position(frames.x, frames.y)
+      frames = seq.next()
+      await sleep(85)
+    }
   }
 }
 
 class Sequence {
-  constructor(xStart, yLine, nextSpriteX) {
+  constructor(xStart, yLine, nextSpriteX, xMax) {
+    this.xStart = xStart
     this.lineY = yLine
     this.nextSpriteX = nextSpriteX
+    this.xMax = xMax
+
     this.x = xStart
     this.y = yLine
   }
 
   next() {
-    // TODO: Add max X, then reset
+    if (Math.sign(this.xMax) == '-1') 
+    {
+      if (this.x <= this.xMax)
+        this.x = this.xStart
+    }
+    else if (this.x >= this.xMax){
+      this.x = this.xStart
+    }
+
     this.x += this.nextSpriteX
     return {
       x: this.x,
@@ -72,9 +79,10 @@ class Sequence {
 }
 
 var jotaro = new Sprite('../dist/sprites/jotaro.png', {width: 100, height: 125});
-var run = new Sequence(-20, -10, 150)
+var run = new Sequence(-20, -10, -125, -800)
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('WE DOING THANGS')
+  console.log('やれやれだぜ。')
 
   jotaro.attach('jotaro')
+  jotaro.play(run)
 })
